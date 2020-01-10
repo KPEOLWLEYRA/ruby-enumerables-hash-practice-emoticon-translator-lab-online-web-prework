69e333cd5ba6a4da7e56db7ac58e2479901fb273
Kelly Powers <kpowers.nj@gmail.com>
2020-01-10 21:09:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 load_library("./lib/emoticons.yml")
 
 def get_japanese_emoticon(library, english_emote)
-  library = load_library("../lib/translator.rb")
+  library = load_library("../lib/emoticons.yml")
   library[:get_emoticon].each do |key, value|
     if english_emote == key
       puts value
